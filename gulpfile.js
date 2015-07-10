af678901045fb5f5b5691ee42490dde1e2103402
Daniel Hägglund <daniel.hagglund@doberman.se>
2015-07-10 15:57:29 +0200

    added gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var rimraf = require('rimraf');
 
+var isProduction = process.argv.indexOf('--prod') >= 0 || process.env.NODE_ENV === 'production';
+
 var errorHandler = function(error) {
   $.util.log(error.toString());
   this.emit('end');
@@ -15,10 +17,13 @@ gulp.task('styles', function() {
   return gulp.src('src/styles/*.scss')
     .pipe($.plumber(errorHandler))
     .pipe($.sass())
+    .pipe($.combineMq())
     .pipe($.autoprefixer())
     .pipe(gulp.dest('build/styles'))
     .pipe($.csso())
-    .pipe($.rename({suffix: '.min'}))
+    .pipe($.rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest('build/styles'))
     .pipe($.connect.reload());
 });
@@ -30,15 +35,28 @@ gulp.task('scripts', function() {
     .pipe($.concat('scripts.js'))
     .pipe(gulp.dest('build/scripts'))
     .pipe($.uglify())
-    .pipe($.rename({suffix: '.min'}))
+    .pipe($.rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest('build/scripts'))
     .pipe($.connect.reload());
 });
 
-gulp.task('html', function() {
+gulp.task('html', ['styles', 'scripts'], function() {
+
+  var injected = gulp.src(['styles/*.css', 'scripts/*.js'], {
+    read: false,
+    cwd: 'build'
+  })
+  .pipe($.filter(isProduction ? '**/*.min.{js,css}' : '**/*[^.min].{js,css}'))
+  .on('data', function(data) {
+    console.log("didn't filter", data.path);
+  })
+
   return gulp.src('src/**/[^_]*.jade')
     .pipe($.plumber(errorHandler))
     .pipe($.jade())
+    .pipe($.inject(injected))
     .pipe(gulp.dest('build'))
     .pipe($.connect.reload());
 });
